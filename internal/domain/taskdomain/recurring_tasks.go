ebfc4f2d9a79145aa4fc8939b98c29af2fa0c9f7
MukizuL <53896777+MukizuL@users.noreply.github.com>
2026-05-08 04:15:46 +0700

    Removed needless json tags

--- internal/domain/taskdomain/recurring_tasks.go
+++ internal/domain/taskdomain/recurring_tasks.go
@@ -12,16 +12,16 @@ const (
 )
 
 type RecurringTask struct {
-	ID          int64      `json:"id"`
-	Title       string     `json:"title"`
-	Description string     `json:"description"`
-	Frequency   Frequency  `json:"frequency"`
-	Interval    int        `json:"interval"`
-	StartDate   time.Time  `json:"start_date"`
-	EndDate     *time.Time `json:"end_date"`
-	LastRunAt   *time.Time `json:"last_run_at"`
-	CreatedAt   time.Time  `json:"created_at"`
-	UpdatedAt   time.Time  `json:"updated_at"`
+	ID          int64
+	Title       string
+	Description string
+	Frequency   Frequency
+	Interval    int
+	StartDate   time.Time
+	EndDate     *time.Time
+	LastRunAt   *time.Time
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
 
 func (f Frequency) Valid() bool {
--- internal/domain/taskdomain/task.go
+++ internal/domain/taskdomain/task.go
@@ -11,14 +11,14 @@ const (
 )
 
 type Task struct {
-	ID              int64     `json:"id"`
-	RecurringTaskID *int64    `json:"recurring_task_id"`
-	Title           string    `json:"title"`
-	Description     string    `json:"description"`
-	Status          Status    `json:"status"`
-	DueDate         time.Time `json:"due_date"`
-	CreatedAt       time.Time `json:"created_at"`
-	UpdatedAt       time.Time `json:"updated_at"`
+	ID              int64
+	RecurringTaskID *int64
+	Title           string
+	Description     string
+	Status          Status
+	DueDate         time.Time
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
 }
 
 func (s Status) Valid() bool {
